416b61be21f2e89a816ec86c0bf22cd611bb2bbb
pelumi-guy <polalekan@gmail.com>
2024-03-31 23:06:57 +0100

    debugInfo uncommented

--- app/actions.ts
+++ app/actions.ts
@@ -7,7 +7,7 @@ export async function sendProposalEmail(formData: FormData) {
   try {
     const debugInfo = await sendEmailService(formData, 'proposal');
     // console.log('sent email...');
-    return ({ success: true, message: 'Sent email successfully' });
+    return ({ success: true, message: 'Sent email successfully', debugInfo });
   } catch (error: any) {
     // throw error;
     return ({ success: false, message: error.message });
@@ -21,7 +21,7 @@ export async function sendContactEmail(formData: FormData) {
   try {
     const debugInfo = await sendEmailService(formData, 'contact');
     // console.log('sent email...');
-    return ({ success: true, message: 'Sent email successfully' });
+    return ({ success: true, message: 'Sent email successfully', debugInfo });
   } catch (error: any) {
     // throw error;
     return ({ success: false, message: error.message });
