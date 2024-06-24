60379f1a4409083a7fe6526db13f61c95f32ff88
Devnuel <emmanuelifegwu03@gmail.com>
2024-06-24 04:27:45 +0100

    members

--- app/api/mail/route.ts
+++ app/api/mail/route.ts
@@ -1,25 +1,28 @@
+// app/api/mail/route.ts
+
 import { EmailTemplate } from '../../../emails/index';
 import { Resend } from 'resend';
+import { NextRequest, NextResponse } from 'next/server';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
+export async function POST(req: NextRequest, res: NextResponse) {
+  const { email, userFirstname } = await req.json();
 
-export async function POST(request:Request, res:Response ) {
-
-  const {email, userFirstname} = await request.json()
   try {
     const { data, error } = await resend.emails.send({
       from: 'EtheredgeMedia <noreply@etheredgemedia.com>',
       to: [email],
       subject: 'Thank you for reaching out',
-      react: EmailTemplate({ userFirstname}),
+      react: EmailTemplate({ userFirstname }),
     });
 
     if (error) {
-      return Response.json({ error }, { status: 500 });
+      return res.json({ error }, { status: 500 });
     }
-    return Response.json({message: "Email sent successfully!"});
+
+    return res.json({ message: "Email sent successfully!" });
   } catch (error) {
-    return Response.json({ error }, { status: 500 });
+    return res.json({ error }, { status: 500 });
   }
 }
